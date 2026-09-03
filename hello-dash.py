58eef074e2a2cd2951c22e67fc2fcb3d70a30ec7
JesusAndres765 <jesusaw065@gmail.com>
2026-09-03 14:56:25 +0000

    edicion de hello py

--- hello-dash.py
+++ hello-dash.py
@@ -1,6 +1,14 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
+
+"""
 st.title("hello streamlit")
 dataframe = pd.read_csv("https://raw.githubusercontent.com/adsoftsito/ciencia-datos/refs/heads/main/titanic.csv")
 st.dataframe(dataframe)
 st.write("by adsoftsito")
+"""
+
+st.title("hello world web")
+st.write("hello world streamlit")
+dataframe = pd.read_csv("https://raw.githubusercontent.com/JesusAndres765/datasets/refs/heads/main/titanic.csv")
+st.dataframe(dataframe)
